c4571d96543bfba755f90a3db09ed2b3b1d6a81f
s1f10230179 <s1f102301796@iniad.org>
2024-07-04 13:37:24 +0900

    flake8エラー修正3

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Task(models.Model):
     title = models.CharField(max_length=100)
     completed = models.BooleanField(default=False)
--- todo/tests.py
+++ todo/tests.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
-# Create your tests here.
+
 class SampleTetCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1 + 2, 3)
